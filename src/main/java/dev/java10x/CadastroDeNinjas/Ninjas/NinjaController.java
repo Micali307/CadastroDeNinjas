db85c4789692ea0b3a28a664d6186cb08647d2c2
Diego Micali <diego.micali13@gmail.com>
2025-03-19 15:19:05 -0300

    feat: atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -11,4 +11,35 @@ public class NinjaController {
         return "Essa é a minha primeira mensagem nessa rota";
     }
 
+//    Adicionar ninja (CREATE)
+    @PostMapping ("/criar")
+    public String criarNinja () {
+        return "Ninja criado";
+    }
+
+//    Mostrar todos os ninjas (READ)
+    @GetMapping ("/todos")
+    public String mostrarTodosOsNinjas () {
+        return "Mostrar ninja";
+    }
+
+//    Mostrar ninja por id (READ)
+    @GetMapping ("/todosID")
+    public String mostrarTodosOsNinjasPorID () {
+        return "Mostrar ninja por ID";
+    }
+
+//    Alterar dados dos ninjas (UPDATE)
+    @PutMapping ("/alterarID")
+    public String alterarNinjaPorID () {
+        return "Alterar Ninja por id";
+    }
+
+//    Deletar Ninja (DELETE)
+    @DeleteMapping ("/deletarID")
+    public String deletarNinjaPorID () {
+        return "Ninja deletado por ID";
+    }
+
+
 }
